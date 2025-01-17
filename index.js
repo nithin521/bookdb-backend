121eb7e9f0c076fd4087646b6fc3483579b628ca
S. Nithin Kumar(66D4) and  Geethanjali College Of Engineering 1st year(AIML-C)  2022-26 <85955710+nithin521@users.noreply.github.com>
2025-01-17 09:37:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://bookconnecttracker.netlify.app'); // Replace with your actual frontend URL
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Allow specific methods
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allow specific headers
+  res.header("Access-Control-Allow-Credentials", "true");
   next();
 });
 //Creation of session
